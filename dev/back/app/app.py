3281e0b362aa9637d4a5dda7dd87c1471e9074ad
Andre Santos Barros da Silva~ <andrezio@live.com>
2022-10-22 21:32:15 -0300

    add cors

--- dev/back/app/app.py
+++ dev/back/app/app.py
@@ -2,10 +2,11 @@ from crypt import methods
 from flask import Flask, jsonify, request, make_response
 from app.database.db import DBConn
 from app.api.users import users_bp
-
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app) # Cors para poder ser utilizado reactjs com front
 app.config['SECRET_KEY'] = 'Projeto Integrador III'
 app.register_blueprint(users_bp)
 
